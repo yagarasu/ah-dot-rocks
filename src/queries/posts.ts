a7f669f7fca5c1d437dff327e1956533f37a3d72
Alexys Hegmann <alexys.hegmann@gmail.com>
2024-11-08 12:43:19 -0600

    Update Single post page + add related posts

--- src/queries/posts.ts
+++ src/queries/posts.ts
@@ -23,3 +23,49 @@ export async function getTags(locale: string = 'es') {
   }
   return tagsMap;
 }
+
+export async function getRelatedPosts(locale: string, entry: { id: string, data: { tags?: string | string[] | null | undefined, category: string | null | undefined, date: Date } }, limit: number = 5) {
+  const posts = await getCollection('blog', (post) => post.data.language === locale);
+  const stats = {
+    similarTags: {
+      min: 0,
+      max: 0
+    },
+    dateDiff: {
+      min: 0,
+      max: 0
+    },
+  }
+  const updateStats = (type: keyof typeof stats, value: number) => {
+    const { min, max } = stats[type];
+    if (value < min) stats[type].min = value;
+    if (value > max) stats[type].max = value;
+  }
+  const index = posts.map((post) => {
+    let percentSimilarTags = 0;
+    if (entry.data.tags !== undefined && entry.data.tags !== null && post.data.tags !== null&& post.data.tags !== undefined) {
+      let similarTags = 0;
+      const postTags = Array.isArray(post.data.tags) ? post.data.tags : [post.data.tags];
+      const entryTags = Array.isArray(entry.data.tags) ? entry.data.tags : [entry.data.tags];
+      similarTags = postTags.reduce((acc, tag) => entryTags.includes(tag) ? acc + 1 : acc, 0);
+      percentSimilarTags = similarTags / postTags.length;
+      updateStats("similarTags", percentSimilarTags);
+    }
+    const dateDiff = Math.abs(entry.data.date.getTime() - post.data.date.getTime());
+    updateStats("dateDiff", dateDiff);
+    return {
+      post,
+      percentSimilarTags,
+      dateDiff,
+      score: 0,
+    }
+  });
+  for (const post of index) {
+    if (post.post.id === entry.id) continue;
+    const scoreSimilarTags = post.percentSimilarTags / stats.similarTags.max;
+    const scoreDateDiff = 1 - ((post.dateDiff - stats.dateDiff.min) / stats.dateDiff.max);
+    post.score = scoreSimilarTags + scoreDateDiff;
+  }
+  index.sort((a, b) => b.score - a.score);
+  return index.slice(0, limit).map(({ post }) => post);
+}
